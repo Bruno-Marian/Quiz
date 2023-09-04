483b9c1fee28baa9c0773681c54c078c5db78a3d
BrunoSM <62313823+Bruno-Marian@users.noreply.github.com>
2023-09-04 20:54:15 -0300

    Ajustes no quiz e adicionado css

--- src/controller/quiz.js
+++ src/controller/quiz.js
@@ -8,7 +8,7 @@ function setaPerguntaQuiz(perguntaIndex){
     html += `<tr>${pergunta}</tr>`
     html += "<br><br>"
     for (let i = 0; i < opcoesList.length; i++) {
-        html += `<button id="${i}" name="${i}" onclick=isCorreto(this.id)>${arrayAlfabeto[i]}) ${opcoesList[i]}</button>`
+        html += `<button id="${i}" name="${i}" class="buttonNew" onclick=isCorreto(this.id)>${arrayAlfabeto[i]}) ${opcoesList[i]}</button>`
         html += "<br><br>"
     }
 
@@ -21,12 +21,12 @@ function setaPerguntaQuiz(perguntaIndex){
 const arrayAlfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
-const gabarito = ['18', '61', "verdadeiro"]
+const gabarito = ['Todas as opções corretas', "Verdadeiro"]
 
-const pergunta1 = ["Qual a sua idade?"]
-const pergunta2 = ["Qual a idade dos seus pais?"]
+const pergunta1 = ["O gerenciamento de projetos é realizado através da aplicação e integração apropriadas dos processos de gerenciamento de projetos identificados para o projeto?"]
+const pergunta2 = ["Qual das opções seriam benefícios do gerenciamento de projetos? "]
 const perguntas = [pergunta1, pergunta2]
 
-const opqst1 = ["20", "25", "18"]
-const opqst2 = ["45", "55", "61"]
+const opqst1 = ["Verdadeiro", "Falso"]
+const opqst2 = ["Cronograma para a realização dos benefícios", "Alinhamento estratégico", "Riscos", "Todas as opções corretas"]
 const opcoes = [opqst1, opqst2]
